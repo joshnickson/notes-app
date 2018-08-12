6e36975d7948e06f5b6d6a0d375fd504bd012abc
josh nickson <jnickson1506@gmail.com>
2018-08-12 10:49:36 +0100

    Note instantiaes with title and body and can be read

--- lib/note.rb
+++ lib/note.rb
@@ -1,2 +1,9 @@
 class Note
+  def initialize(title, body)
+    @title = title
+    @body = body
+  end
+  def read
+    [@title, @body]
+  end
 end
--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -2,4 +2,9 @@ require 'note'
 
 describe Note do
   
+  it 'instantiates with title and body' do
+    note = Note.new('title', 'body')
+    expect(note.read).to eq ['title', 'body']
+  end
+
 end
